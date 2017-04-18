59760d00d5d8fd71661123b120686649861be3f2
kecorbin <kecorbin@cisco.com>
2017-04-17 21:24:06 -0500

    add oauth integration for updating member list

--- app.py
+++ app.py
@@ -1,23 +1,139 @@
-from flask import Flask, render_template, Markup
-from flask_restful import Api, Resource, request
-import BeautifulSoup
-import requests
 import os
+import requests
 
-# TODO need to add data persistence
+from flask import Flask, request, g, session, request, redirect, url_for, render_template, Markup
+from flask import render_template_string
+from flask_restful import Api, Resource, request
 
-USERS = [{"githubuser": "kecorbin"}]
+from flask_github import GitHub
 
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
+DATABASE_URI = 'sqlite:////tmp/github-flask.db'
+SECRET_KEY = 'development key'
+DEBUG = True
+
+# test data
+USERS = []
+
+# Set these values
+GITHUB_CLIENT_ID = os.getenv("CLIENT_ID")
+GITHUB_CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+
+# setup flask
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+# setup github-flask
+app.config['GITHUB_CLIENT_ID'] = os.getenv("CLIENT_ID")
+app.config['GITHUB_CLIENT_SECRET'] = os.getenv("CLIENT_SECRET")
+
+github = GitHub(app)
+
+#github.authorize('admin:org')
+SCOPE = 'read:org'
+# setup flask-restful
 api = Api(app)
 
+# setup sqlalchemy
+engine = create_engine(app.config['DATABASE_URI'])
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+# Helpers for Github OAuth integration
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(200))
+    github_access_token = Column(String(200))
+
+    def __init__(self, github_access_token):
+        self.github_access_token = github_access_token
+
+
+@app.before_request
+def before_request():
+    g.user = None
+    if 'user_id' in session:
+        g.user = User.query.get(session['user_id'])
+
+
+@app.after_request
+def after_request(response):
+    db_session.remove()
+    return response
+
+
+@github.access_token_getter
+def token_getter():
+    user = g.user
+    if user is not None:
+        return user.github_access_token
+
+@app.route('/success')
+@github.authorized_handler
+def success():
+    global USERS
+    members = github.get('orgs/imapex/members')
+    member_ids = [m['login'] for m in members]
+    USERS = [{"githubuser": str(u)} for u in member_ids]
+    print USERS
+
+    return render_template_string("User list updated based on org membership!")
+
+
+@app.route('/authorized')
+@github.authorized_handler
+def authorized(access_token):
+    next_url = request.args.get('next') or url_for('success')
+    if access_token is None:
+        return redirect(next_url)
+
+    user = User.query.filter_by(github_access_token=access_token).first()
+
+    if user is None:
+        user = User(access_token)
+        db_session.add(user)
+    user.github_access_token = access_token
+    db_session.commit()
+
+    session['user_id'] = user.id
+    print("Updating users requested by: {}".format(session['user_id']))
+    return redirect(next_url)
+
+
+@app.route('/update')
+def login():
+    return github.authorize(scope=SCOPE)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return redirect(url_for('index'))
+
+
+@app.route('/user')
+def user():
+    return str(github.get('user'))
+
+
 @app.route('/')
-def contributions():
+def index():
+
     cals = {}
     for u in USERS:
         resp = requests.get('https://github.com/users/{}/contributions'.format(u['githubuser'])).text
-        soup = BeautifulSoup.BeautifulSoup(resp)
         cals[u['githubuser']] = Markup(resp)
 
     return render_template('index.html', cals=cals)
@@ -34,5 +150,7 @@ class AddMember(Resource):
 
 api.add_resource(AddMember, '/api/users/add')
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=os.getenv("FLASK_DEBUG"))
+    init_db()
+    app.run(debug=True)
